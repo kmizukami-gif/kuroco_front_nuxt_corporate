a20f0c6979811ffdf60ef3df603c2f870b92dd72
kmizukami-gif <kmizukami@diverta.co.jp>
2025-09-02 14:53:21 +0900

    Update index.vue

--- pages/privacy/index.vue
+++ pages/privacy/index.vue
@@ -19,7 +19,7 @@ const { data: response } = await useFetch(
   `${config.public.kurocoApiDomain}/rcms-api/1/content/details/privacy`,
   {
     credentials: "include",
-    server: false,
+
   }
 );
 </script>
